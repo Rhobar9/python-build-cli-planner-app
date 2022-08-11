5a61f634b875f26f7cafa15af128816e146e4620
Rhobar9 <olo.k97@wp.pl>
2022-08-11 22:21:13 +0200

    After_package_refactoring

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from src.database import add_reminder, list_reminders
-from src.deadlined_reminders import DateReminder, DeadlinedReminder
+from deadlined_reminders import DateReminder, DeadlinedReminder
 from src.external_reminders import EveningReminder
 from src.reminder import PoliteReminder
 
